7c6f0e7fc45518420a82aaefb4e33a68aeb49dbb
Admin-Pc\Admin <gimalai18@gmail.com>
2021-04-14 13:56:16 +0300

    Lesson_2

---
+++ Lesson_2/Rectangle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter numbers of colums ");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number sof lines ");
+            int y = int.Parse(Console.ReadLine());
+            int a = 0;
+
+            while (a < x)
+            {
+                
+                int b = 0;
+                while (b < y)
+                {
+                    Console.Write("*");
+                    b++;
+                }
+
+            Console.WriteLine();
+            a++;
+                        
+            }
+        }
+    }
+}
---
+++ Lesson_2/Rhombus/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Rhombus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number rows ");
+            int rows = int.Parse(Console.ReadLine());
+            int space = 0;
+            int star = 1;
+            int y = 0;
+            int x = 0;
+
+            while (y < rows)
+            {
+                while (space < rows)
+                {
+                    Console.Write(" ");
+                    space++;
+                }
+                x = 0;
+                while (x < star)
+                {
+                    Console.Write("*");
+                    x++;
+                }
+                x = 0;
+                while (x < star - 1)
+                {
+                    Console.Write("*");
+                    x++;
+                }
+                Console.WriteLine();
+                space = star++;
+                y++;
+            }
+
+            space = 0;
+            star = 2;
+            y = 0;
+            x = rows;
+
+            while (star < rows + 1)
+            {
+                while (space < star)
+                {
+                    Console.Write(" ");
+                    space++;
+                }
+                while (space < rows)
+                {
+                    Console.Write("*");
+                    space++;
+                }
+                while (y < x - 1)
+                {
+                    Console.Write("*");
+                    y++;
+                }
+                Console.WriteLine();
+                star++;
+                space = 0;
+                y = 0;
+                x--;
+            }
+            Console.ReadLine();
+        }
+    }
+}
