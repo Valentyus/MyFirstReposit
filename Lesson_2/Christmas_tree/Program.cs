e4de3c19df221bd8e9aa5dd0bbf99f23250f814c
Valentyus <82445962+Valentyus@users.noreply.github.com>
2021-04-14 15:37:54 +0300

    Add files via upload

---
+++ Lesson_2/Christmas_tree/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Christmas_tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number rows ");
+            int rows = int.Parse(Console.ReadLine());
+            int space = 0;
+            int star = 1;
+            int y = 0;
+
+            while (y < rows)
+            {
+                while (space < rows)
+                {
+                    Console.Write(" ");
+                    space++;
+                }
+                int x = 0;
+                while (x < star - 1)
+                {
+                    Console.Write("*");
+                    x++;
+                }
+                x = 0;
+                while (x < star)
+                {
+                    Console.Write("*");
+                    x++;
+                }
+                Console.WriteLine();
+                space = star++;
+                y++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson_2/Triangles/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Triangles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите высоту треугольника: ");
+            int rows = int.Parse(Console.ReadLine());
+            Console.WriteLine("Треугольник 1\n");
+            int lattice = 1;
+            int space = 0;
+            int numbRows = 0;
+
+            while (numbRows < rows)
+            {
+                space = 0;
+                while (space < lattice)
+                {
+                    Console.Write("#");
+                    space++;
+                }
+                lattice++;
+                Console.WriteLine();
+                numbRows++;
+            }
+
+            Console.WriteLine("\nТреугольник 2\n");
+
+            numbRows = 0;
+            lattice = rows;
+            while (numbRows < rows)
+            {
+                space = 0;
+                while (space < lattice)
+                {
+                    Console.Write("#");
+                    space++;
+                }
+                Console.WriteLine();
+                numbRows++;
+                lattice--;
+            }
+
+            Console.WriteLine("\nТреугольник 3\n");
+
+            numbRows = 0;
+            lattice = 0;
+            while (numbRows < rows)
+            {
+                space = 0;
+                while (space < lattice)
+                {
+                    Console.Write(" ");
+                    space++;
+                }
+                for (int unit = numbRows; unit < rows; unit++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+                lattice++;
+                numbRows++;
+            }
+
+            Console.WriteLine("\nТреугольник 4\n");
+
+            numbRows = 0;
+            lattice = rows;
+            while (numbRows < rows)
+            {
+                space = 1;
+                while (space < lattice)
+                {
+                    Console.Write(" ");
+                    space++;
+                }
+                for (int unit = 0; unit <= numbRows; unit++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+                numbRows++;
+                lattice--;
+            }
+        }
+    }
+}
